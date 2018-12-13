2dfa284824e41dbd75c9d1734b9c51811c56b94a
Anders Ryden <rydena021@gmail.com>
2018-12-13 15:52:39 -0600

    adds semicolon to sql query, removes unused Link in app, and exports navbar

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ CREATE TABLE "favorites" (
     "id" SERIAL PRIMARY KEY,
     "image_url" VARCHAR (250) NOT NULL,
     "category_id" INT DEFAULT NULL
-)
+);
 
 -- Category table
 CREATE TABLE "category" (
