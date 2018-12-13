1af93f26e1981498d47704afc760cf32b5de4d6a
Amie Thao <amie.c.thao@gmail.com>
2018-12-13 14:34:22 -0600

    add search result reducers

--- src/redux/reducers/searchResults.reducer.js
+++ src/redux/reducers/searchResults.reducer.js
@@ -1,5 +1,11 @@
+
 const searchResults = (state = [], action) =>{
-  
+  switch(action.type) {
+    case 'SET_GIFS':
+      return action.payload
+    default:
+      return state
+  }
 }
 
 export default searchResults;
