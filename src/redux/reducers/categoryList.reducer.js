acd69ac5cd57d30802423ac6ddaf83f1afa4d11a
Travis Lang <tlang50@icloud.com>
2018-12-13 16:00:23 -0600

    add categories saga&reducer

---
+++ src/redux/reducers/categoryList.reducer.js
@@ -0,0 +1,8 @@
+const categoryList = (state = [], action) => {
+    switch(action.type) {
+        case 'SET_CATEGORIES':
+            return action.payload
+        default:
+            return state
+    }
+}
