c10ff966574c7601bde365a7aabda32db4c37f0b
Anders Ryden <rydena021@gmail.com>
2018-12-13 14:38:19 -0600

    adds favorite item component

---
+++ src/components/Favorites/FavoriteItem.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class FavoriteItem extends Component {
+
+  render() {
+    // individual favorite object, given via props
+    // let favorite = this.props.favorite;
+    let favorite = {
+      image_url: "https://media0.giphy.com/media/1d5Tfv7yEy2tSztA5k/giphy.webp",
+      category: 'funny'
+    }
+    // array of categories, given via redux
+    // let categories = this.props.categories
+    let categories = ['funny', 'nsfw']
+    // map through categories, create option for each
+    let categoryHtml = categories.map( (category, i) => {
+      return <option key={i} value={category}>{category}</option>
+    });
+
+    return (
+      <div>
+        <img src={favorite.image_url} alt="favorite gif" />
+        <select>
+          {categoryHtml}
+        </select>
+        <button>Add Category</button>
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = reduxState => ({
+  categories: reduxState.categoryList,
+});
+
+export default connect(mapStateToProps)(FavoriteItem);
---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FavoriteItem from './FavoriteItem';
+
+class Favorites extends Component {
+  componentDidMount() {
+    this.props.dispatch({ type: 'FETCH_FAVORITES' });
+  }
+
+  render() {
+    let favoritesHtml = this.props.favorites.map(favorite => <FavoriteItem key={favorite.id} favorite={favorite} />);
+    return (
+      <div>
+        {favoritesHtml}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = reduxState => ({
+  favorites: reduxState.favoriteList,
+});
+
+export default connect(mapStateToProps)(Favorites);
