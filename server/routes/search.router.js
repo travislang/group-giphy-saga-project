ad02756b8e77396190436fb0c8a061382030e5a9
Travis Lang <tlang50@icloud.com>
2018-12-13 14:28:03 -0600

    add saga for giphy api call

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', (req, res) => {
-
+    let searchTerm = req.query.term;
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}`)
+    .then( response => {
+        res.send(response.data)
+    }).catch( err => {
+        console.log('error in api GET:', err);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router;
