b8c8e33484f89a30d2249916fc5acc5dff7542dd
Anders Ryden <rydena021@gmail.com>
2018-12-14 11:23:47 -0600

    adds material ui card to favorite items

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import FavoriteItem from './FavoriteItem';
+import FavoriteItem from './Test';
 
 class Favorites extends Component {
   componentDidMount() {
---
+++ src/components/Favorites/Test.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FavoriteItemForm from './FavoriteItemForm';
+import { withStyles } from '@material-ui/core/styles';
+import { Delete } from '@material-ui/icons';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 190,
+  },
+};
+
+class FavoriteItem extends Component {
+
+  handleClick = event => {
+    this.props.dispatch({ type: 'REMOVE_FAVORITE', payload: this.props.favorite.id });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={this.props.favorite.image_url}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Category: {this.props.favorite.name}
+          </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <FavoriteItemForm favId={this.props.favorite.id} />
+          <Button size="small" color="default" onClick={this.handleClick}>
+            <Delete />
+          </Button>
+        </CardActions>
+      </Card>
+
+    );
+  }
+}
+
+export default connect()(withStyles(styles)(FavoriteItem));
