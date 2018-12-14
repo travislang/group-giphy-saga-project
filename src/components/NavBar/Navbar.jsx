9c40c4f6ebd05ac91b3c35bb619040cff5920024
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2018-12-14 11:22:40 -0600

    navbar complete

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,18 +1,155 @@
-import React, { Component } from 'react';
-import { withRouter, Link} from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Navbar</h1>
-        <Link to='/'>Home</Link>
-        <Link to='/favorites'>Favorites</Link>
-      </div>
-    );
-  }
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing.unit * 2,
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing.unit * 3,
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing.unit * 9,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+});
+
+class Navbar extends React.Component {
+
+    getGifs = () => {
+        console.log();
+        this.props.dispatch({ type: 'FETCH_GIFS', payload: this.state.search });
+    }
+
+    handleChangeFor = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log('testing handlechangefor', this.state.search);
+    }
+    state = {
+        anchorEl: null,
+        mobileMoreAnchorEl: null,
+        search: ''
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                        </IconButton>
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                            Giphy
+                        </Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                name="search"
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                onChange={this.handleChangeFor}
+                            />
+                        </div>
+                 
+                            <Button variant="contained" color="secondary" className={classes.button} onClick={this.getGifs}>
+                                Search
+                     </Button>
+                    
+                    <Button variant="contained" color="secondary"><Link to="/">Home</Link></Button>
+                    <Button variant="contained" color="secondary"><Link to="/favorites">Favorites</Link></Button>
+                    
+                        
+                        <div className={classes.sectionMobile}>
+
+                        </div>
+                    </Toolbar>
+                </AppBar>
+
+            </div>
+        );
+    }
 }
 
+Navbar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
+export default withRouter(connect()(withStyles(styles)(Navbar)));
 
-export default withRouter(Navbar);
