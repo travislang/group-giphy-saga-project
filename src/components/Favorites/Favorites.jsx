96e49d10d4b6f10a96cdbe5f4c82965c3f455821
Anders Ryden <rydena021@gmail.com>
2018-12-14 09:16:04 -0600

    adds material ui to favorite component

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FavoriteItem from './FavoriteItem';
+import Test from './FavoriteItemForm';
 
 class Favorites extends Component {
   componentDidMount() {
